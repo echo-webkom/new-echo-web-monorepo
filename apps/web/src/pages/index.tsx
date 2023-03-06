d4d37c9b3514b55f16907bd71f00bb2b62ca42ea
Ole Magnus Fon Johnsen <me@omfj.no>
2023-03-06 15:57:36 +0100

    feat: conditionally show footer sectins

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,49 +1,29 @@
-import Link from "next/link";
-import {useState} from "react";
-import * as Switch from "@radix-ui/react-switch";
+import Image from "next/image";
 
-import {Layout} from "@/components";
+import {Button, Layout} from "@/components";
 import {fetchEventPreviews} from "@/api";
 
-interface Props {
-  eventPreviews: Awaited<ReturnType<typeof fetchEventPreviews>>;
-}
-
-const HomePage = ({eventPreviews}: Props) => {
-  const [checked, setChecked] = useState(true);
-
+const HomePage = () => {
   return (
     <Layout>
-      <div className="container mx-auto">
-        <div className="flex gap-3">
-          <span>Vis bedpresser:</span>{" "}
-          <Switch.Root
-            onCheckedChange={(b) => setChecked(b)}
-            defaultChecked={checked}
-            className="relative h-[25px] w-[42px] cursor-default rounded-full bg-gray-200 outline-none focus:shadow-[0_0_0_2px] focus:shadow-blue-400 data-[state=checked]:bg-echo-yellow"
-          >
-            <Switch.Thumb className="block h-[21px] w-[21px] translate-x-0.5 rounded-full bg-white shadow-[0_2px_2px] shadow-blackA7 transition-transform duration-100 will-change-transform data-[state=checked]:translate-x-[19px]" />
-          </Switch.Root>
+      <section className="container mx-auto flex items-center justify-between px-20">
+        <div className="relative h-64 w-64">
+          <Image
+            height={512}
+            width={512}
+            alt="logo"
+            src="/images/android-chrome-512x512.png"
+            className="object-cover"
+          />
         </div>
-
-        {checked && (
-          <>
-            <h2 className="text-3xl font-bold">Bedpresser</h2>
-            <ul className="flex flex-col gap-3">
-              {eventPreviews.map((event) => (
-                <li key={event._id}>
-                  <div className="rounded-md bg-black/5 p-5">
-                    <h3 className="text-xl font-bold">{event.title}</h3>
-                    <Link className="underline" href={`/event/${event.slug}`}>
-                      Les mer!
-                    </Link>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </>
-        )}
-      </div>
+        <div>
+          <h2 className="text-2xl">echo - linjeforeningen for informatikk</h2>
+          <div>
+            <Button>Arrangementer</Button>
+            <Button>For bedrifter</Button>
+          </div>
+        </div>
+      </section>
     </Layout>
   );
 };
